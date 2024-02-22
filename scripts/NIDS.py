98668ae6ccc07cdf6739bec00095cf831eca3722
HyungJoon <itineris24@naver.com>
2024-02-22 01:20:15 -0800

    modified version for handling csv

--- scripts/NIDS.py
+++ scripts/NIDS.py
@@ -1,14 +1,15 @@
 import pandas as pd
-from scripts import objectCSV as ref
+from scripts import preprocess as ref
 
 ORIGINAL_CSV = '../data/UNSW-NB15-BALANCED-TRAIN.csv'
 
 
 def main():
     # Read csv using pandas in Latin mode
-    df = pd.read_csv(ORIGINAL_CSV, encoding='ISO-8859-1', low_memory=False)
-    df2 = ref.refactor_data(df)
-    print(df2.dtypes)
+    origin = pd.read_csv(ORIGINAL_CSV, encoding='ISO-8859-1', low_memory=False)
+    df = ref.preprocess_data(origin)
+
+    print(df.info())
 
     return 0
 
